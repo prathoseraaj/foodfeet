5f99f219281452c2d576d5bdee37e47c56cbacce
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-22 18:25:10 +0530

    feat: header nd css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite Food Here</h2>
+            <p>your ultimate destination for satisfying cravings! We deliver delicious meals, fresh flavors, and happiness right to your doorstep. From local favorites to international cuisines, enjoy seamless ordering, fast delivery, and top-notch customer service. Your next meal is just a click away!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Header/>
+    </div>
   )
 }
 
