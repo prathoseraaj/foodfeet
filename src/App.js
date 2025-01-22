0fff1526fd3eebfaf687996c61fd73bac71a63ea
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-22 16:34:19 +0530

    feat: Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Placeorder from './pages/Placeorder/Placeorder';
+import Home from './pages/home/Home';
+import Cart from './pages/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        < Route path='' element={<Home/>}/>
+        <Route path='' element={<Cart/>}/>
+        <Route path='' element={<Placeorder/>}/>
+      </Routes>
     </div>
   );
 }
