e805faacb62af4f5428bf97e1d129e005818c23b
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-23 11:02:18 +0530

    feat: updated context api

--- src/Context/Storecontext.jsx
+++ src/Context/Storecontext.jsx
@@ -1,13 +1,29 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
+    const [cartitems, setCartitems] = useState({});
+    const addtocart = (itemId) =>{
+        if(!cartitems[itemId]){
+            setCartitems((prev)=>({...prev,itemId:1}))
+        }
+        else{
+            setCartitems((prev)=>({...prev,[itemId]: prev[itemId]+1}))
+        }
+    }
+    const removefromcart = (itemId) => {
+        setCartitems((prev) => ({...prev,[itemId]:[itemId]-1}))
+    }
 
     const contextvalue = {
-        food_list
+        food_list,
+        cartitems,
+        setCartitems,
+        addtocart,
+        removefromcart,
     }
     return(
         <StoreContext.Provider value={contextvalue}>
