3f6d2c7c1acd71419d86591056e5ad9d2da8b184
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-23 15:31:09 +0530

    feat: loginpopup

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Placeorder from './pages/Placeorder/Placeorder';
 import Home from './pages/home/Home';
 import Cart from './pages/Cart/Cart';
 import Footer from './components/footer/Footer';
+import { useState } from 'react';
+import Loginpopup from './components/loginpopup/Loginpopup';
 
 function App() {
+  const [showlogin, setShowlogin] = useState(false);
   return (
     <>
+    {showlogin?<Loginpopup setShowlogin={setShowlogin}/>:<></>}
       <div className="App">
-        <Navbar/>
+        <Navbar setShowlogin={setShowlogin}/>
         <Routes>
           < Route path='' element={<Home/>}/>
           <Route path='' element={<Cart/>}/>
---
+++ src/components/loginpopup/Loginpopup.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Loginpopup.css'
+import { assets } from '../../assets/frontend_assets/assets';
+
+const Loginpopup = ({setShowlogin}) => {
+    const [currentstate,setCurrentstate] = useState('Sign up');
+  return (
+    <div className='loginpopup'>
+        <form className='login-popup'>
+            <h2>Sign Up</h2>
+            <img onClick={()=>setShowlogin(false)} src={assets.cross_icon} alt="" />
+            <div className='loginpopup-input'>
+                {currentstate==='Sign up'?<input type="text" placeholder='Name' required /> : <></> }
+            <input type="text" placeholder='Username' required/>
+            <input type="text" placeholder='Password' required/>
+            </div>
+            <button>{currentstate === 'Sign up'? 'Create Account' : 'Login In'}</button>
+            <div className='login-popup-condition'>
+                <input type="checkbox" required />
+                <p>By countinuing, I agree to the terms of Use & Privacy policy</p>
+            </div>
+            {currentstate==='Sign up'? <p>Already have an Account? <span onClick={()=>setCurrentstate('Login In')}>Login here</span></p> 
+                                     : <p>Create a new Account? <span onClick={()=>setCurrentstate('Sign up')}>click here</span></p> }
+        </form>
+    </div>
+  )
+}
+
+export default Loginpopup
